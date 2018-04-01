0c94fe99cedda60a503a107c2bdd16aa1b073789
Shin'ya Ueoka <ueokande@i-beam.org>
2018-04-01 12:31:49 +0900

    migrate to kafka 1.0.0 on word count example

--- src/main/java/com/shapira/examples/streams/wordcount/WordCountExample.java
+++ src/main/java/com/shapira/examples/streams/wordcount/WordCountExample.java
@@ -3,9 +3,10 @@ package com.shapira.examples.streams.wordcount;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.Materialized;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -17,23 +18,23 @@ public class WordCountExample {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
-        KStreamBuilder builder = new KStreamBuilder();
-        KStream<String, String> source =
-                builder.stream("wordcount-input");
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("wordcount-input");
+
         final Pattern pattern = Pattern.compile("\\W+");
         KStream counts = source
                 .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
                 .map((key, value) -> new KeyValue<Object, Object>(value, value))
                 .filter((key, value) -> (!value.equals("the")))
                 .groupByKey()
-                .count("CountStore")
+                .count(Materialized.as("CountStore"))
                 .mapValues(value -> Long.toString(value)).toStream();
         counts.to("wordcount-output");
 
-        KafkaStreams streams = new KafkaStreams(builder, props);
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
         // usually the stream application would be running forever,
         // in this example we just let it run for some time and stop since the input data is finite.
