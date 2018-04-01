e3530a84720dae5040bd43e0f8e4c022f5d5a22e
Shin'ya Ueoka <ueokande@i-beam.org>
2018-04-01 22:01:58 +0900

    clean clickstream

--- src/main/java/com/shapira/examples/streams/clickstreamenrich/ClickstreamEnrichment.java
+++ src/main/java/com/shapira/examples/streams/clickstreamenrich/ClickstreamEnrichment.java
@@ -9,12 +9,15 @@ import com.shapira.examples.streams.clickstreamenrich.serde.JsonSerializer;
 import com.shapira.examples.streams.clickstreamenrich.serde.WrapperSerde;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.Properties;
 
@@ -37,20 +40,20 @@ public class ClickstreamEnrichment {
         // don't use in large production apps - this increases network load
         // props.put(CommonClientConfigs.METADATA_MAX_AGE_CONFIG, 500);
 
-        KStreamBuilder builder = new KStreamBuilder();
+        StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<Integer, PageView> views = builder.stream(Serdes.Integer(), new PageViewSerde(), Constants.PAGE_VIEW_TOPIC);
-        KTable<Integer, UserProfile> profiles = builder.table(Serdes.Integer(), new ProfileSerde(), Constants.USER_PROFILE_TOPIC, "profile-store");
-        KStream<Integer, Search> searches = builder.stream(Serdes.Integer(), new SearchSerde(), Constants.SEARCH_TOPIC);
+        KStream<Integer, PageView> views = builder.stream(Constants.PAGE_VIEW_TOPIC, Consumed.with(Serdes.Integer(), new PageViewSerde()));
+        KTable<Integer, UserProfile> profiles = builder.table(Constants.USER_PROFILE_TOPIC, Consumed.with(Serdes.Integer(), new ProfileSerde()));
+        KStream<Integer, Search> searches = builder.stream(Constants.SEARCH_TOPIC, Consumed.with(Serdes.Integer(), new SearchSerde()));
 
         KStream<Integer, UserActivity> viewsWithProfile = views.leftJoin(profiles,
-                    (page, profile) -> {
-                        if (profile != null)
-                            return new UserActivity(profile.getUserID(), profile.getUserName(), profile.getZipcode(), profile.getInterests(), "", page.getPage());
-                        else
-                           return new UserActivity(-1, "", "", null, "", page.getPage());
+                (page, profile) -> {
+                    if (profile != null)
+                        return new UserActivity(profile.getUserID(), profile.getUserName(), profile.getZipcode(), profile.getInterests(), "", page.getPage());
+                    else
+                        return new UserActivity(-1, "", "", null, "", page.getPage());
 
-        });
+                });
 
         KStream<Integer, UserActivity> userActivityKStream = viewsWithProfile.leftJoin(searches,
                 (userActivity, search) -> {
@@ -60,11 +63,12 @@ public class ClickstreamEnrichment {
                         userActivity.updateSearch("");
                     return userActivity;
                 },
-                JoinWindows.of(1000), Serdes.Integer(), new UserActivitySerde(), new SearchSerde());
+                JoinWindows.of(1000),
+                Joined.with(Serdes.Integer(), new UserActivitySerde(), new SearchSerde()));
 
-        userActivityKStream.to(Serdes.Integer(), new UserActivitySerde(), Constants.USER_ACTIVITY_TOPIC);
+        userActivityKStream.to(Constants.USER_ACTIVITY_TOPIC, Produced.with(Serdes.Integer(), new UserActivitySerde()));
 
-        KafkaStreams streams = new KafkaStreams(builder, props);
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
 
         streams.cleanUp();
 
@@ -80,26 +84,26 @@ public class ClickstreamEnrichment {
     }
 
     static public final class PageViewSerde extends WrapperSerde<PageView> {
-        public PageViewSerde() {
-            super(new JsonSerializer<PageView>(), new JsonDeserializer<PageView>(PageView.class));
+        PageViewSerde() {
+            super(new JsonSerializer<>(), new JsonDeserializer<>(PageView.class));
         }
     }
 
     static public final class ProfileSerde extends WrapperSerde<UserProfile> {
-        public ProfileSerde() {
-            super(new JsonSerializer<UserProfile>(), new JsonDeserializer<UserProfile>(UserProfile.class));
+        ProfileSerde() {
+            super(new JsonSerializer<>(), new JsonDeserializer<>(UserProfile.class));
         }
     }
 
     static public final class SearchSerde extends WrapperSerde<Search> {
-        public SearchSerde() {
-            super(new JsonSerializer<Search>(), new JsonDeserializer<Search>(Search.class));
+        SearchSerde() {
+            super(new JsonSerializer<>(), new JsonDeserializer<>(Search.class));
         }
     }
 
     static public final class UserActivitySerde extends WrapperSerde<UserActivity> {
-        public UserActivitySerde() {
-            super(new JsonSerializer<UserActivity>(), new JsonDeserializer<UserActivity>(UserActivity.class));
+        UserActivitySerde() {
+            super(new JsonSerializer<>(), new JsonDeserializer<>(UserActivity.class));
         }
     }
 }
